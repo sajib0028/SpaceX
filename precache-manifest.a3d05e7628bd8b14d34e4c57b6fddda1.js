40f87a8a3a9c9713946efda0ef6716ca37e839ee
Sajib132 <sajibb813@gmail.com>
2021-03-10 14:36:30 +0530

    Updates

---
+++ precache-manifest.a3d05e7628bd8b14d34e4c57b6fddda1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f12f1bb1b12da30dfd29802e2c91a8a3",
+    "url": "/SpaceX/index.html"
+  },
+  {
+    "revision": "a8fa0fc6419624aee851",
+    "url": "/SpaceX/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "b93ea8da76c0dce0d33f",
+    "url": "/SpaceX/static/css/main.e141296e.chunk.css"
+  },
+  {
+    "revision": "a8fa0fc6419624aee851",
+    "url": "/SpaceX/static/js/2.6cdbf0dc.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/SpaceX/static/js/2.6cdbf0dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b93ea8da76c0dce0d33f",
+    "url": "/SpaceX/static/js/main.9e1b92ff.chunk.js"
+  },
+  {
+    "revision": "dbbc612723065eb6204d",
+    "url": "/SpaceX/static/js/runtime-main.60a47493.js"
+  },
+  {
+    "revision": "c537d7d180515a2d14e723821dcf5097",
+    "url": "/SpaceX/static/media/loadRocket.c537d7d1.gif"
+  }
+]);
